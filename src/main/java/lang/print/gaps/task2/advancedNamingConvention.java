469f602811f79c498c9687a41040be7ce05240c3
zurazumba <zura.zumbadze@yahoo.com>
2023-06-12 20:27:30 +0400

    third

--- src/main/java/lang/print/gaps/task2/advancedNamingConvention.java
+++ src/main/java/lang/print/gaps/task2/advancedNamingConvention.java
@@ -1,16 +1,15 @@
 package lang.print.gaps.task2;
-
 public class advancedNamingConvention {
-    private static final int adult_age = 18;
+    private static final int ADULT_AGE = 18;
 
-    private int AGE;
+    private int age;
     private int phoneNumber;
 
-    void call_to_friend() {
-        //some code
+    void callToFriend() {
+        // Some code
     }
 
-    void callByNumber(int phoneNumber) {
-        //some code
+    void callByNumber(int number) {
+        // Some code
     }
 }
